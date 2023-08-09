1df7489f2ab3b3253dcd5a33f1e31201de3ab2d6
Mohamed Waked <mwaked01@gmail.com>
2023-08-09 01:39:19 -0400

    created a connection to the server and log a message from teh server

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: '::',
+    port: 50541,
+  });
+  
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //logs messages coming from the server
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  
+  return conn;
+};
+
+
+console.log("Connecting ...");
+connect();
